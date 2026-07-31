65f0f7b6675719f23a73871c21c54d5fd6aea2b3
AndiEcker <aecker2@gmail.com>
2026-07-31 13:30:57 +0100

    v0.3.10: final docstring fixes to prepare ae-ae namespace RTD build
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  ae/system.py
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.87
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.91
 """ setup of ae namespace module portion system: Python system helpers. """
 import pathlib
 import sys
@@ -75,7 +75,7 @@ setup_kwargs: dict[str, Any] = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/ae-group/ae_system',
-    'version': '0.3.9',
+    'version': '0.3.10',
     'zip_safe': True,
 }
 
